80ed13071ec922b21882fd14027ceef9e55222ee
caprocoo <dhgustjd10@gmail.com>
2022-11-27 15:57:00 +0900

    ~ TodoInput 컴포넌트 코드 정리 및 UI 스타일링

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    똔꼬몬
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    기여운
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
